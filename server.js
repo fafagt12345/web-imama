9cbe22551c166516bae8a5cf642a60e85203673c
fafagt12345 <azrofafathun77@gmail.com>
2026-05-07 17:35:07 +0700

    Fix static routing and remove duplicate admin file

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use(express.static(__dirname));
+
 // Konfigurasi Koneksi Database
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
